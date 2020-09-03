c98d2b5c7f53749c7ad23196a2cd97a54ae3e68b
Mohammad Alshoubaki <mohammad.z.alshoubaki@gmail.com>
2020-09-03 09:40:47 -0400

    lesson data types and vars | working with numbers

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -14,5 +14,27 @@ namespace Hello_World
             string userAnswer = Console.ReadLine();
             Console.WriteLine($"That's cool.. You said: '{ userAnswer}'");
         }
+        static void dataTypesCC()
+        {
+            // first part
+            Console.Write("Enter your favorite number!: ");
+            int faveNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write(faveNumber - 10);
+
+
+            // working with numbers
+            double numberOne = 6.5;
+            double numberTwo = 4;
+
+            // Raise numberOne to the numberTwo power
+            Console.WriteLine(Math.Pow(numberOne, numberTwo));
+
+            // Round numberOne up
+            Console.WriteLine(Math.Ceiling(numberOne));
+
+            // Find the larger number between numberOne and numberTwo
+            Console.WriteLine(Math.Max(numberOne, numberTwo));
+
+        }
     }
 }
